8cd4f4b28551a58c5a5467115b2d19d8d1e5710f
Nakanis <73628253+Nakanis@users.noreply.github.com>
2020-12-13 10:15:19 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+'use strict';
 
+const target = document.getElementById('target');
+
+target.addEventListener('click', () =>{
+  // document.getElementById('target').style.background = 'pink';
+  // document.getElementById('target').style.borderRadius = '50%';
+  target.classList.toggle('circle');
+} );
