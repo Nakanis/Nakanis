ea1a3343201a0f8d994e40831d2e8077e31be85e
Nakanis <73628253+Nakanis@users.noreply.github.com>
2021-01-18 18:20:28 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,31 +1,30 @@
-'use strict';
-
-
-{
-  const btn = document.getElementById('btn');
-  const text = document.getElementById('text');
-
-
-  btn.addEventListener('click', () => {
-    const n = Math.floor(Math.random() * 3);
-  btn.textContent = n;
-
-  switch(n){
-    case 0:
-      btn.textContent = '大吉';
-      text.textContent = '割りばしがきれいに割れるようになるでしょう';
-      break;
-    case 1:
-      btn.textContent = '中吉';
-      text.textContent = '人生楽しもうぜ';
-      break;
-    case 2:
-      btn.textContent = '凶';
-      text.textContent = 'PCのスペースキーが壊れるでしょう';
-      break;
-  }
-  });
+var character = document.getElementById("chacter");
+var block = document.getElementById("block");
+function jump(){
+  character.classList.add("animate");
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
